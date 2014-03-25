de1cad4dd809d063ef7e996a68b835783cef5a2b
sjs7007 <sjs7007@gmail.com>
2014-03-25 17:31:44 +0530

    Node1

--- Final.py
+++ Final.py
@@ -5,7 +5,7 @@ import sys
 
 #ISBN="0441172717"
 ISBN=sys.argv[1]
-APIKey="X" #Enter your own key here
+APIKey="G0YLe4KJic94J0Bi0zeBQ" #Enter your own key here
 url="https://www.goodreads.com/book/isbn?isbn="+ISBN+"&key="+APIKey
 
 #PageSource=urllib2.urlopen("https://www.goodreads.com/book/isbn?isbn=0441172717&key=X").read()
@@ -16,23 +16,30 @@ text_file.close()
 
 #now read from xml file and display output
 
+output=""
 xmldoc = minidom.parse('temp.xml')
 itemlist = xmldoc.getElementsByTagName('original_title') 
 print "Title :-"
 print itemlist[0].childNodes[0].nodeValue
 print "-------------"
+output=output+itemlist[0].childNodes[0].nodeValue+"\n"
 
 xmldoc = minidom.parse('temp.xml')
 itemlist = xmldoc.getElementsByTagName('description') 
 print "Description :-"
 print itemlist[0].childNodes[0].nodeValue
 print "-------------"
+output=output+itemlist[0].childNodes[0].nodeValue+"\n"
 
 itemlist = xmldoc.getElementsByTagName('average_rating') 
 print "Average Rating :-"
 print itemlist[0].childNodes[0].nodeValue
 print "-------------"
+output=output+itemlist[0].childNodes[0].nodeValue+"\n"
 
+text_file = open("output.txt", "w")
+text_file.write("%s" %output)
+text_file.close()
 
 
 
