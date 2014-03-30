0b61286e75f4676b555f8ae22b9fbc3f0930e0f2
sjs7007 <sjs7007@gmail.com>
2014-03-30 22:51:29 +0530

    basic web app done

--- NodeJSLocal/app2.js
+++ NodeJSLocal/app2.js
@@ -4,7 +4,6 @@ var exec = require("child_process").exec;
 
 app.use(express.bodyParser());
 
-
 app.get('/hello.txt', function(req, res){
 	res.send('Hello World');
 });
@@ -12,17 +11,6 @@ app.get('/hello.txt', function(req, res){
 app.get('/Output.xml', function(req, res){
 	res.set('Content-Type','text/xml');
 	res.sendfile('Output.xml');
-});
-
-app.get('/ISBNSearch', function(req, res){
-	console.log("Request handler 'ISBNSearch' was called.");
-	var content = "empty";
-	exec("python Final.py 9780007369683", function (error, stdout, stderr) {
-	content = stdout;
-	res.send(content);
-
-
-	});
 });
 
 
@@ -33,24 +21,13 @@ app.get('/', function (req, res)
     res.render('StartPage.html');
 });
 
-app.get('/v2', function (req, res)
+app.post('/v2', function (req, res)
 {
-    res.render('Final2.html');
+    var command = "python Final.py "+req.body.isbnNumber;
+    exec(command, function (error, stdout, stderr) {
+    	res.render('Final2.html');
+	});
 });
-
-app.get('/v3', function (req, res)
-{
-    res.render('Final3.html');
-});
-
-
-
-
-app.post('/myaction', function(req, res) {
-  console.log(req.body.emailJK);
-  res.send(req.body.emailJK)
-});
-
 
 
 var server = app.listen(3000, function() {
