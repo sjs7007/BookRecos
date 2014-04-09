5e1559321547526d203733cb3b6c24689dc29fe9
sjs7007 <sjs7007@gmail.com>
2014-04-09 19:58:51 +0530

    Debugging code removed.

--- recoSite.py
+++ recoSite.py
@@ -21,26 +21,17 @@ readDB(allUsers,'BookDB.csv')
 
 Booklists=['BookName','Asylum','Fire and blood','Twisted sisters','Moth and Spark','Dangerous Illusions','The Black-Eyed Blonde','The ghost runner','Dead silent','Before we met','I forgot to remember','Sixth extinction','Wild Things','Alienated','Annhilation','Flight of the silvers','Influx','Tuesdays with Morrie','The alchemist','The fountainhead','Jonathan Livingston Seagull','Mahashweta','Illusions','One','The sound of letting go','Sherlock Holmes and the Vampires of London']
 ISBN=['ISBN','1599907844','0825460131','0451239652','0670015709','1940521645','0805098143','1620403404','1408327562','1620402750','1451685815','0805092994','0451415191','1423170288','0374104093','0399164987','0525953183','0751529818','0061122416','0451191153','0743278909','8126417463','0099427869','0330311735','0670015539','1616552662']
+
 def equation1(x,y): #x=actual rating,y=true rating
 	x_y = x - y
 	partPersonality = math.exp(-(x_y)*(x_y)/12.5) # 12.5 = 2 sigma^2 where sigma = free parameter
 	return partPersonality
-answer='Y' #for while loop to rate multiple books
+
 nUsers = 30 #number of users
 nReviews = 25 #number of reviews
 Final=['FinalRatings','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null']#list of final predicted ratings for active user
 currentUser=['ActiveUser','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null']
 probabilityList = [] #probability that current user is similar to ith user
-"""while(answer=='Y'):
-print("To proceed further kindly rate the books you've read on a scale of 1-5\n1.Asylum\n2.Fire and blood\n3.Twisted sisters\n4.Moth and Spark\n5.Dangerous Illusions\n6.The Black-Eyed Blonde\n7.the ghost runner\n8.Dead silent\n9.Before we met\n10.I forgot to remember\n11.Sixth extinction\n12.Wild Things\n13.Alienated\n14.Annhilation\n15.Flight of the silvers\n16.Influx\n17.Tuesdays with Morrie\n18.The alchemist\n19.The fountainhead\n20.Jonathan Livingston Seagull\n21.Mahashweta\n22.Illusions\n23.One\n24.The sound of letting go\n25.Sherlock Holmes and the Vampires of London")
-num=int(raw_input("Enter a book number"))
-print "Enter your rating for book" , num
-rating=int(raw_input(""))
-	currentUser[num]=rating
-	Final[num]=rating
-	answer=raw_input("want to rate another book? press Y/N")
-print("To proceed further kindly rate the books you've read on a scale of 1-5\n1.Asylum\n2.Fire and blood\n3.Twisted sisters\n4.Moth and Spark\n5.Dangerous Illusions\n6.The Black-Eyed Blonde\n7.the ghost runner\n8.Dead silent\n9.Before we met\n10.I forgot to remember\n11.Sixth extinction\n12.Wild Things\n13.Alienated\n14.Annhilation\n15.Flight of the silvers\n16.Influx\n17.Tuesdays with Morrie\n18.The alchemist\n19.The fountainhead\n20.Jonathan Livingston Seagull\n21.Mahashweta\n22.Illusions\n23.One\n24.The sound of letting go\n25.Sherlock Holmes and the Vampires of London")
-"""
 
 countRatings = len(sys.argv)-1 
 current = 1
@@ -48,7 +39,6 @@ while(current<=countRatings):
 	currentUser[int(sys.argv[current])]=sys.argv[current+1]
 	Final[int(sys.argv[current])]=sys.argv[current+1]
 	current=current+2
-
 
 for i in range (0,nUsers):
 	probabilityList.append(1)
@@ -65,10 +55,6 @@ for i in range (0,nUsers):
 	else:
 		probabilityList[i] = 0
 
-#print "Just printed PL"
-#print probabilityList
-
-#booknumber=int(raw_input("Enter book number for which you want predicted rating"))
 for x in range(1,24):
 	if currentUser[x]=="null":
 	 	booknumber=x
@@ -77,7 +63,7 @@ for x in range(1,24):
 			probabilityDistResult.append(0)
 			for j in range (0,nUsers):
 				x = i+1 # i+1 for rating 1,2,3,4,5 
-				y = float(allUsers[j][booknumber]) #assume 3rd movie's rating we are finding out for now
+				y = float(allUsers[j][booknumber]) #jth movie's rating we are finding out for now
 				probabilityDistResult[i] = probabilityDistResult[i] + equation1(x,y) * probabilityList[j]
 
 		temp = probabilityDistResult[0]
@@ -87,10 +73,8 @@ for x in range(1,24):
 				temp = probabilityDistResult[i]
 				finalRating = i+1
 
-		#print "Final rating for book", booknumber, Booklists[booknumber],"with ISBN", ISBN[booknumber]
 		Final[booknumber]=finalRating
-		#print finalRating
-#print("Final Rating : $d" %finalRating)
+
 
 root = ET.Element("root")
 doc = ET.SubElement(root, "books")
